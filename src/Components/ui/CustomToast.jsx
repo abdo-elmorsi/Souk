a41b4be63d5ce20ef2e07fb2a72e48248790ef57
Abdo2711-coder <abdelrahmandiv@gmail.com>
2022-01-25 03:54:14 +0200

    dd

--- src/Components/ui/CustomToast.jsx
+++ src/Components/ui/CustomToast.jsx
@@ -1,29 +1,24 @@
 import toast from 'react-hot-toast'
 
-
-const CustomToast = ({t, message}) => {
+const CustomToast = ({ t, message }) => {
     return (
-       
-            <div
-              className={`${
+        <div
+            className={`${
                 t.visible ? 'animate-enter' : 'animate-leave'
-              } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-            >
-              <div className="flex-1 w-0 p-4">
-             <p className="text-base font-bold text-red-500">
-                 {message}
-             </p>
-              </div>
-              <div className="flex border-l border-gray-200">
-                <button
-                  onClick={() => toast.dismiss(t.id)}
-                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                >
-                  Close
-                </button>
-              </div>
+            } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+        >
+            <div className="flex-1 w-0 p-4">
+                <p className="text-base font-bold text-red-500">{message}</p>
             </div>
-        
+            <div className="flex border-l border-gray-200">
+                <button
+                    onClick={() => toast.dismiss(t.id)}
+                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                    Close
+                </button>
+            </div>
+        </div>
     )
 }
 
